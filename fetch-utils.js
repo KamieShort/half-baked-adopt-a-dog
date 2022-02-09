bde6a95e60a44ed3d308fbf2fdd9a1b82085d3e4
Kamie Short <kamie.short@gmail.com>
2022-02-09 11:53:21 -0800

    initial commit

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,22 +1,25 @@
-const SUPABASE_URL = 'https://gxwgjhfyrlwiqakdeamc.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA';
+const SUPABASE_URL = "https://gxwgjhfyrlwiqakdeamc.supabase.co";
+const SUPABASE_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA";
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export async function getDogs() {
-    // from the dogs table, select all items
+  // from the dogs table, select all items
+  const resp = await client.from("dogs").select("*");
+  // console.log(resp);
 
-    // and return the response (checking for errors)
-    return checkError(response);    
+  // and return the response (checking for errors)
+  return checkError(resp);
 }
 
 export async function getDog(id) {
-    // from the dogs table, select a single dog who has the matching id
-
-    // and return the response (checking for errors)
-    return checkError(response);    
+  // from the dogs table, select a single dog who has the matching id
+  const resp = await client.from("dogs").select("*").eq("id", id).single();
+  // and return the response (checking for errors)
+  return checkError(resp);
 }
 
 function checkError({ data, error }) {
-    return error ? console.error(error) : data;
+  return error ? console.error(error) : data;
 }
