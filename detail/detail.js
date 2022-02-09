addc9f285777dd624323dc75c505119313e1c8b8
Kamie Short <kamie.short@gmail.com>
2022-02-09 15:17:52 -0800

    details page complete

--- detail/detail.js
+++ detail/detail.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { getDog } from "../fetch-utils.js";
 import { renderDogDetail } from "../render-utils.js";
 
@@ -6,7 +7,7 @@ console.log("is this working from details page?");
 // console.log(window.location.search);
 
 // on load
-window.addEventListener("load", async () => {
+window.addEventListener("load", async() => {
   // get the id from URL
   const params = new URLSearchParams(window.location.search);
   const dog = await getDog(params.get("id"));
--- fetch-utils.js
+++ fetch-utils.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const SUPABASE_URL = "https://gxwgjhfyrlwiqakdeamc.supabase.co";
 const SUPABASE_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA";
@@ -17,6 +18,7 @@ export async function getDog(id) {
   // from the dogs table, select a single dog who has the matching id
   const resp = await client.from("dogs").select("*").eq("id", id).single();
   // and return the response (checking for errors)
+  console.log(resp);
   return checkError(resp);
 }
 
